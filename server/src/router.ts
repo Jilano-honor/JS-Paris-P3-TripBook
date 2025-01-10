31f88045eb6db83081b419c1aa8f61d6616affb9
Julien <juju.gruet@mail.com>
2025-01-10 11:44:48 +0100

    server

--- server/src/router.ts
+++ server/src/router.ts
@@ -9,6 +9,10 @@ const router = express.Router();
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
 
+/*router.get("/api/Theme/:id/Tags/:id/Coutries/Country/:id", CountryActions.browse);*/
+/*router.get("/api/Theme/:id/Tags/:id/Coutries", CountriesActions.browse);*/
+/*router.get("/api/Theme", ThemeActions.browse);*/
+
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
