362a43755589c5e445f4c25a816f1034980ec8d8
Julien <juju.gruet@mail.com>
2025-01-16 15:18:26 +0100

    a

--- server/src/modules/FilterTag/filtertagAction.ts
+++ server/src/modules/FilterTag/filtertagAction.ts
@@ -12,24 +12,18 @@ const browse: RequestHandler = async (req, res, next) => {
 
 const read: RequestHandler = async (req, res, next) => {
 	try {
-		const countryId = Number(req.params.id);
-
 		const tagIds = req.query.tag_ids
 			? Array.isArray(req.query.tag_ids)
 				? req.query.tag_ids.map(Number)
 				: [Number(req.query.tag_ids)]
 			: [];
 
-		const filteredCountry = await filtertagRepository.readByTags(tagIds);
+		const filteredCountries = await filtertagRepository.readByTags(tagIds);
 
-		const filteredCountriesByCountryId = filteredCountry.filter(
-			(entry) => entry.country_id === countryId,
-		);
-
-		if (filteredCountriesByCountryId.length === 0) {
+		if (filteredCountries.length === 0) {
 			res.sendStatus(404);
 		} else {
-			res.json(filteredCountriesByCountryId);
+			res.json(filteredCountries);
 		}
 	} catch (err) {
 		next(err);
